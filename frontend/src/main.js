15636de1298674abf88aab64c2391a758bf4af41
ouvh <oussamalaaroussiFCB@gmail.com>
2025-04-30 09:14:52 +0100

    working but need inhancement specially for the metrics , maybe consider adding plots

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import io from 'socket.io-client'
 import App from './App.vue'
-import './assets/main.css'
 
 // Import Tailwind CSS
 import './assets/css/main.css'
